cd70671d72abbac2db877c93804220083e3d6743
Yusuf.Q <yusufqing920@gmail.com>
2019-12-30 01:47:12 +0800

    application middleware

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello Project.');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
