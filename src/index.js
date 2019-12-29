029803994cf37cbe8e2e30a1e243b1d6d820fed2
Yusuf.Q <yusufqing920@gmail.com>
2019-12-30 05:46:06 +0800

    graphql schema and resolver

--- src/index.js
+++ src/index.js
@@ -1,15 +1,111 @@
 import 'dotenv/config';
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 import cors from 'cors';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
-
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+const schema = gql`
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+    messages: [Message!]!
+    message(id: ID!): Message!
+  }
+  type User {
+    id: ID!
+    username: String!
+    messages: [Message!]
+  }
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
+  }
+`;
 
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
-);
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+      messageIds: [1],
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+      messageIds: [2],
+    },
+  };
+  let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+  };
+  const me = users[1];
+
+  const resolvers = {
+    Query: {
+        users: () => {
+            return Object.values(users);
+          },
+      user: (parent, { id }) => {
+        return users[id];
+      },
+      me: (parent, args, { me }) => {
+        return me;
+      },
+      messages: () => {
+        return Object.values(messages);
+      },
+      message: (parent, { id }) => {
+        return messages[id];
+      },
+    },
+    Mutation: {
+        createMessage: (parent, { text }, { me }) => {
+          const id = uuidv4();
+          const message = {
+            id,
+            text,
+            userId: me.id,
+          };
+          return message;
+        },
+      },
+    
+    User: {
+        messages: user => {
+          return Object.values(messages).filter(
+            message => message.userId === user.id,
+          );
+        },
+      },
+      Message: {
+        user: message => {
+          return users[message.userId];
+        },
+      },
+  };
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    me: users[1],
+  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
